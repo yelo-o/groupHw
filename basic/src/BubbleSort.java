9ca03b5ff481fb34ebbf7306f6feb7961e72bac4
yelo-o <flyordig@gmail.com>
2023-05-24 14:11:39 +0900

    BubbleSort, SelectionSort 클래스 초기화

--- basic/src/BubbleSort.java
+++ basic/src/BubbleSort.java
@@ -1,12 +1,2 @@
-public class BubbleSort extends Sort {
-	void sort(int[] arr) {
-		System.out.println("버블정렬 진행합니다");
-		for(int i=0;i<arr.length;i++) {
-			for(int j=1;j<arr.length-i;j++) {
-				if(arr[j-1] > arr[j]) {
-					swap(arr,(j-1),j);
-				}
-			}
-		}
-	}
+public class BubbleSort {
 }
--- basic/src/SelectionSort.java
+++ basic/src/SelectionSort.java
@@ -1,14 +1,2 @@
-public class SelectionSort extends Sort {
-	void sort(int[] arr) {
-		System.out.println("선택정렬 진행합니다");
-		for(int i=0;i<arr.length;i++) {
-			int indexMin = i;
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[indexMin] > arr[j]) {
-					indexMin=j;
-				}
-			}
-			swap(arr,indexMin,i);
-		}
-	}
+public class SelectionSort {
 }
